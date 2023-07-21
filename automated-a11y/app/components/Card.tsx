4dd330a7b3f4e9e0f8483fb72bb708529eefede0
Andrej <a.arsenovski@labdigital.nl>
2023-07-21 16:00:38 +0200

    remove unncessary files

--- automated-a11y/app/components/Card.tsx
+++ automated-a11y/app/components/Card.tsx
@@ -1,3 +1,3 @@
 export const Card = () => {
-  return <h2>Helloooo!!!</h2>;
+  return <h2>This is a simple card!!!</h2>;
 };
--- automated-a11y/app/page.tsx
+++ automated-a11y/app/page.tsx
@@ -4,7 +4,6 @@ import { Card } from "./components/Card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card></Card>
     </main>
   );
 }
