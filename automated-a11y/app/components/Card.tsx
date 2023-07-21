6452dc47a256a9f28637796a3411535b7f97ef28
Andrej <a.arsenovski@labdigital.nl>
2023-07-21 15:49:27 +0200

    add automated exercises

---
+++ automated-a11y/app/components/Card.tsx
@@ -0,0 +1,3 @@
+export const Card = () => {
+  return <h2>Helloooo!!!</h2>;
+};
---
+++ automated-a11y/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Card } from "./components/Card";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Card></Card>
+    </main>
+  );
+}
